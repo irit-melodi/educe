d6693118f16e6a66c1477f2f0a92775f8a98b9c4
Eric Kow <eric.kow@gmail.com>
2013-02-05 19:58:18 +0000

    Distutils stub

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='educe',
+      version='0.1',
+      author='Eric Kow',
+      author_email='eric.kow@gmail.com',
+      #py_modules=['educe'],
+      scripts=['glozz.py'],
+      requires=[]
+      )
