29591e556e4271efa0156359b830de4e1120941a
Eric Kow <eric.kow@gmail.com>
2013-02-27 14:05:51 +0100

    Move educe.glozz model into educe.annotation
    My current thinking is to have two abstract layers:
    - annotation
    - graph
    
    We only have the annotation layer for now.  The way I current see things
    evolving in the future
    
    Abstract layer:
    
    -    **educe.annotation**: low-level, close to annotations
    -    **educe.graph**:  high level, graph-like structure
    -    **educe.corpus**: abstraction over corpus organisation
    
    Format/formalism layer:
    
    -    **educe.glozz** : read Glozz XML files into educe.annotation
    -    **educe.sdrt** : convert educe.annotation to educe.graph
         (cf a possible educe.rst, which does same)
    
    Data-set layer:
    
    *    **educe.stac**: things which are specific to a particular
         data set; encodes filename conventions and various
         task-specific/ad-hoc choices made along the way
         (cf a possible educe.annodis...)

--- educe/glozz.py
+++ educe/glozz.py
@@ -4,121 +4,11 @@
 import xml.etree.ElementTree as ET
 import sys
 
+from educe.annotation import *
+
 # Glozz annotations
-# The aim here is to be fairly low-level and just model how glozz
-# thinks about things
-# If you want to do any more sophisticated analysis, you'll probably
-# want to build a more abstract layer on top of it, eg. using the
-# python graph library
-
-class Span:
-    def __init__(self, start, end):
-        self.char_start=start
-        self.char_end=end
-
-    def  __str__(self):
-        return ('(%d,%d)' % (self.char_start, self.char_end))
-
-class RelSpan(Span):
-    def __init__(self, t1, t2):
-        self.t1=t1
-        self.t2=t2
-
-    def  __str__(self):
-        return ('%s -> %s' % (self.t1, self.t2))
-
-class Unit:
-    def __init__(self, unit_id, span, type, features, origin=None):
-        self.origin=origin
-        self.__unit_id=unit_id
-        self.span=span
-        self.type=type
-        self.features=features
-
-    def __str__(self):
-        feats=", ".join(map(str,self.features))
-        return ('%s [%s] %s %s' % (self.identifier(),self.type, self.span, feats))
-
-
-    def position(self):
-        """
-        The position is the set of "geographical" information only to identify
-        an item. So instead of relying on some sort of name, we might rely on
-        its text span. We assume that some name-based elements (document name,
-        subdocument name, stage) can double as being positional.
-
-        If the unit has an origin (see "FileId"), we use the
-
-        * document
-        * subdocument
-        * stage
-        * (but not the annotator!)
-        * and its text span
-
-        ** position vs identifier **
-
-        This is a trade-off.  One the hand, you can see the position as being
-        a safer way to identify a unit, because it obviates having to worry
-        about your naming mechanism guaranteeing stability across the board
-        (eg. two annotators stick an annotation in the same place; does it have
-        the same name). On the *other* hand, it's a bit harder to uniquely
-        identify objects that may coincidentally fall in the same span.  So
-        how much do you trust your IDs?
-        """
-        o=self.origin
-        if o is None:
-            ostuff=[]
-        else:
-            ostuff=[o.doc, o.subdoc, o.stage]
-        return ":".join(ostuff + map(str,[self.span.char_start, self.span.char_end]))
-
-    def identifier(self):
-        """
-        String representation of an identifier that should be unique
-        to this corpus at least.
-
-        If the unit has an origin (see "FileId"), we use the
-
-        * document
-        * subdocument
-        * stage
-        * (but not the annotator!)
-        * and the id from the XML file
-
-        If we don't have an origin we fall back to just the id provided
-        by the XML file
-
-        See also `position` as potentially a safer alternative to this
-        (and what we mean by safer)
-        """
-        o=self.origin
-        if o is None:
-            ostuff=[]
-        else:
-            ostuff=[o.doc, o.subdoc, o.stage]
-        return ":".join(ostuff + [self.__unit_id])
-
-class Relation(Unit):
-    def __init__(self, rel_id, span, type, features):
-        Unit.__init__(self, rel_id, span, type, features)
-
-class Feature():
-    """Attribute-value pair"""
-    def __init__(self, attribute, value):
-        self.attribute=attribute
-        self.value=value
-
-    def __str__(self):
-        if self.value is None:
-            return self.attribute
-        else:
-            return ('%s:%s' % (self.attribute,self.value))
-
-class Annotations:
-    def __init__(self, units, rels):
-        self.units=units
-        self.rels=rels
-
+# The aim here is to be fairly low-level and just capture how glozz
+# thinks about things.
 
 # ---------------------------------------------------------------------
 # xml processing
@@ -216,7 +106,7 @@ def read_node(node, context=None):
 def read_annotation_file(filename):
     tree = ET.parse(filename)
     res  = read_node(tree.getroot())
-    return Annotations(res[0],res[1])
+    return Document(res[0],res[1])
 
 def slurp_corpus(cfiles, verbose=False):
     """
