ab38d7bd0c8fba7392cf5bd31a73d51018eca77f
Eric Kow <eric.kow@gmail.com>
2013-10-04 16:07:27 +0100

    Represent RST treebank trees as educe Standoff

--- educe/rst_dt/parse.py
+++ educe/rst_dt/parse.py
@@ -47,7 +47,7 @@ test="""
 )
 """
 
-test_text=\
+test_text="".join(\
         [" ORGANIZING YOUR MATERIALS ",
          " Once you've decided on the kind of paneling you want to install --- and the pattern ---",
          "some preliminary steps remain",
@@ -57,7 +57,7 @@ test_text=\
          "buy the paneling,",
          "gather the necessary tools and equipment (see illustration on page 87),",
          "and even condition certain types of paneling before installation. "
-         ]
+         ])
 
 test0="""
 ( Root (span 5 6)
@@ -71,14 +71,19 @@ import re, sys
 import codecs
 from nltk import Tree
 
+from educe.external.parser import SearchableTree
+from educe.annotation      import Standoff, Span
+
 # pre-processing leaves
 text_re = re.compile(r"\(text (?P<text>.+(</EDU>|</s>|_!))\)")
 
 # pre-processing heads
-type_re = r"\((?P<type>(Nucleus|Satellite))"
+type_re      = r"\((?P<type>(Nucleus|Satellite))"
+root_type_re = r"\((?P<type>(Root))"
 span_re = r"\((?P<span>(leaf [0-9]+|span [0-9]+ [0-9]+))\)"
 rel_re  = r"\((?P<rel>rel2par [\-A-Za-z0-9:]+)\)"
-head_pattern = re.compile("%s %s %s"%(type_re,span_re,rel_re))
+head_pattern = re.compile("%s %s %s" % (type_re,      span_re, rel_re))
+root_pattern = re.compile("%s %s"    % (root_type_re, span_re))
 
 # parsing
 leaf_pattern = r"\[[^\]]+\]" # non-']' chars in square brackets
@@ -89,56 +94,19 @@ def process_text(matchobj):
 
 def process_head(matchobj):
     ntype = matchobj.group("type")
-    span = matchobj.group("span").replace(" ","-")
-    rel = matchobj.group("rel").split()[1]
+    span  = matchobj.group("span").replace(" ","-")
+    rel   = "---" if ntype == "Root" else matchobj.group("rel").split()[1]
     return "(%s|%s|%s"%(ntype,span,rel)
 
 def mark_leaves(str):
     return text_re.sub(process_text,str)
 
 def mark_heads(str):
-   return head_pattern.sub(process_head,str)
+    s = root_pattern.sub(process_head,str)
+    return head_pattern.sub(process_head,s)
 
-def preprocess(str):
-    """
-    Given a raw RST treebank string, return a massaged representation for easier
-    parsing, along with its first/last EDU number.
-
-    The string is assumed to take the form "(Root (span X Y) Nodes.. )", where X
-    and Y are integers and Nodes are RST tree string nodes
-    """
-    s_     = preprocess_helper(str)
-    root, span, minedu_, maxedu_, s = s_.split(" ",4)
-    tstr   = "%s %s" % (root, s)
-    minedu = int(minedu_)
-    maxedu = int(maxedu_[:-1]) # trailing right bracket
-    return tstr, minedu, maxedu
-
-def preprocess_helper(str):
-    """
-    Convert a raw RST treebank tree string to something which is a bit easier
-    for us to parse
-    """
-    res = str.strip()
-    res = mark_leaves(res)
-    res = re.sub(r"\(\s+","(",res)#.replace("\n",""))
-    res = re.sub(r"\s+\)",")",res)
-    res = re.sub(r"\s\s+"," ",res)
-    res = mark_heads(res)
-    return res
-
-def postprocess(tree):
-    if isinstance(tree,Tree):
-        children = [postprocess(child) for child in tree] 
-        return Tree(Node(tree.node),children)
-    else:
-        if tree.startswith("["):
-            return EDU(tree[1:-1])
-        else:
-            raise Exception( "ERROR in rst tree format for leaf : ", child)
-
-class EDU:
-    def __init__(self,descr):
+class EDU(Standoff):
+    def __init__(self, descr, start=0, origin=None):
         s = descr.strip()
         if s.startswith("<s>"):
             self._sentstart = True
@@ -150,45 +118,106 @@ class EDU:
             s = s[:-4]
         else:
             self._sentend = False
-        # remove <EDU></EDU> mark
-        self._text = s[5:-6]
+
+        self.text   = s[5:-6] # remove <EDU></EDU> mark
+        end         = start + len(self.text)
+        self.span   = Span(start, end) # text-span (not the same as EDU span)
+        Standoff.__init__(self, origin)
 
     def __repr__(self):
-        return self._text
+        return self.text
 
 class Node:
-    def __init__(self,descr):
-        if descr =="span" or descr=="Root":
-            self.type = "root"
-            self.span = "---"
-            self.rel = None
-        else:
-            self.type, self.span, self.rel = descr.split("|")
-            self.span = self.span.split("-")
-            if len(self.span)==2:
-                self.span.append(self.span[1])
-            self.span[1] = int(self.span[1] )
-            self.span[2] = int(self.span[2] )
-
+    def __init__(self,descr,span, origin=None):
+        self.type, self.edu_span, self.rel = descr.split("|")
+        self.edu_span = self.edu_span.split("-")
+        if len(self.edu_span)==2:
+            self.edu_span.append(self.edu_span[1])
+        self.edu_span[1] = int(self.edu_span[1] )
+        self.edu_span[2] = int(self.edu_span[2] )
+        # Standoff
+        self.span = span
 
     def __repr__(self):
-        return "%s %s %s"%(self.type,"%s-%s"%tuple(self.span[1:3]),self.rel)
+        return "%s %s %s"%(self.type,"%s-%s"%tuple(self.edu_span[1:3]),self.rel)
 
-class RSTTree(Tree):
+class RSTTree(SearchableTree, Standoff):
+    def __init__(self,node,children,origin=None):
+        """
+        Note, you should use `RSTTree.build(str)` to create this tree
+        instead
+        """
+        SearchableTree.__init__(self, node, children)
+        Standoff.__init__(self, origin)
 
-    def __init__(self,str):
-        tstr, minedu, maxedu = preprocess(str)
-        t_  = Tree.parse(tstr, leaf_pattern=leaf_pattern)
-        t   = postprocess(t_)
-        self._minedu = minedu
-        self._maxedu = maxedu
-        Tree.__init__(self, t.node, t)
+    def text_span(self):
+        return self.node.span
+
+    def _members(self):
+        return list(self) # children
 
     def __repr__(self):
         return self.pprint()
 
-    def span(self):
-        return self._minedu,self._maxedu
+    def edu_span(self):
+        """
+        Return the span of the tree in terms of EDU count
+        See `self.span` refers more to the character offsets
+        """
+        minedu = self.node.edu_span[1]
+        maxedu = self.node.edu_span[2]
+        return minedu,maxedu
+
+    def text(self):
+        """
+        Return the text corresponding to this RST tree
+        (traverses and concatenates leaf node text)
+        """
+        return "".join(l.text for l in self.leaves())
+
+    @classmethod
+    def build(cls, str):
+        tstr = cls._preprocess(str)
+        t_   = Tree.parse(tstr, leaf_pattern=leaf_pattern)
+        return cls._postprocess(t_)
+
+    @classmethod
+    def _preprocess(cls, str):
+        """
+        Helper function: Given a raw RST treebank string, return a massaged
+        representation for easier parsing, along with its first/last EDU number.
+        """
+        res = str.strip()
+        res = mark_leaves(res)
+        res = re.sub(r"\(\s+","(",res)#.replace("\n",""))
+        res = re.sub(r"\s+\)",")",res)
+        res = re.sub(r"\s\s+"," ",res)
+        res = mark_heads(res)
+        return res
+
+    @classmethod
+    def _postprocess(cls, tree, start=0):
+        """
+        Helper function: Convert the NLTK-parsed representation of an RST tree
+        to one using educe-style Standoff objects
+        """
+        if isinstance(tree,Tree):
+            children = []
+            position = start
+            for c in tree:
+                child    = cls._postprocess(c, position)
+                children.append(child)
+                child_sp = child.node.span if isinstance(child,Tree) else child.span
+                position = child_sp.char_end
+
+            span = Span(start, position)
+            return RSTTree(Node(tree.node, span),children)
+        else:
+            if tree.startswith("["):
+                return EDU(tree[1:-1], start)
+            else:
+                raise Exception( "ERROR in rst tree format for leaf : ", child)
+
 
 def read_annotation_file(anno_filename):
     """
@@ -196,5 +225,5 @@ def read_annotation_file(anno_filename):
     """
     t = None
     with codecs.open(anno_filename, 'r', 'utf-8') as tf:
-        t = RSTTree(tf.read())
+        t = RSTTree.build(tf.read())
     return t
--- educe/rst_dt/tests.py
+++ educe/rst_dt/tests.py
@@ -4,17 +4,16 @@ import sys
 
 from educe.rst_dt import parse
 
-def test_preprocess():
-    tstr, minedu, maxedu = parse.preprocess(parse.test)
-    assert minedu == 1
-    assert maxedu == 9
-
 def test_builtin():
-    parse.RSTTree(parse.test0)
-    t = parse.RSTTree(parse.test)
-    leaf_text = [ l._text for l in t.leaves() ]
-    assert parse.test_text == leaf_text
+    parse.RSTTree.build(parse.test0)
+    t      = parse.RSTTree.build(parse.test)
+    t_text = t.text()
+    sp     = t.node.span
+    assert t.edu_span()    == (1,9)
+    assert parse.test_text == t_text
+    assert sp.char_end     == len(t_text)
 
 def test_from_files():
     for i in glob.glob('tests/rst*.dis'):
-        parse.read_annotation_file(i)
+        t      = parse.read_annotation_file(i)
+        assert t.node.span.char_end == len(t.text())
