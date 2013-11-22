b3337afdde2a7d0fedc7ad1f303522970802e021
Eric Kow <eric.kow@gmail.com>
2013-11-22 11:19:12 +0100

    Whoops, also install educe.{rst_dt,pdtb} subpkgs

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(name='educe',
       version='0.1',
       author='Eric Kow',
       author_email='eric.kow@gmail.com',
-      packages=['educe', 'educe.stac', 'educe.external'],
+      packages=['educe', 'educe.stac', 'educe.rst_dt', 'educe.pdtb', 'educe.external'],
       scripts=['glozz', 'glozz-graph'],
       requires=['python_graph (>= 1.8.2)', 'pydot', 'python_graph_dot']
       )
