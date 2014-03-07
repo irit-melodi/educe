6628bba43ece6bd452f2613866fdb8fa578cf731
Eric Kow <eric.kow@gmail.com>
2014-03-07 10:34:01 +0000

    rst_dt: Re-export to avoid code in __init__.py
    This is what I actually wanted to do in the first place,
    but I didn't understand the Python module system well
    enough to do at the time

---
+++ educe/rst_dt/corpus.py
@@ -0,0 +1,72 @@
+# Author: Eric Kow
+# License: BSD3
+
+"""
+Corpus management (re-exported by educe.rst_dt)
+"""
+
+from glob import glob
+import os
+import sys
+
+from educe.corpus import FileId
+from educe.rst_dt import parse
+import educe.corpus
+
+# ---------------------------------------------------------------------
+# Corpus
+# ---------------------------------------------------------------------
+
+
+class Reader(educe.corpus.Reader):
+    """
+    See `educe.corpus.Reader` for details
+    """
+    def __init__(self, corpusdir):
+        educe.corpus.Reader.__init__(self, corpusdir)
+
+    def files(self):
+        anno_files = {}
+        full_glob = os.path.join(self.rootdir, '*.dis')
+
+        for fname in glob(full_glob):
+            bname = os.path.basename(fname)
+            doc = os.path.splitext(bname)[0]
+            k = FileId(doc=doc,
+                       subdoc=None,
+                       stage='discourse',
+                       annotator='None')
+            anno_files[k] = fname
+        return anno_files
+
+    def slurp_subcorpus(self, cfiles, verbose=False):
+        """
+        See `educe.rst_dt.parse` for a description of `RSTTree`
+        """
+        corpus = {}
+        counter = 0
+        for k in cfiles.keys():
+            if verbose:
+                sys.stderr.write("\rSlurping corpus dir [%d/%d]" %
+                                 (counter, len(cfiles)))
+            anno_file = cfiles[k]
+            annotations = parse.read_annotation_file(anno_file)
+            annotations.set_origin(k)
+            corpus[k] = annotations
+            counter = counter+1
+        if verbose:
+            sys.stderr.write("\rSlurping corpus dir [%d/%d done]\n" %
+                             (counter, len(cfiles)))
+        return corpus
+
+
+def id_to_path(k):
+    """
+    Given a fleshed out FileId (none of the fields are None),
+    return a filepath for it following RST Discourse Treebank
+    conventions.
+
+    You will likely want to add your own filename extensions to
+    this path
+    """
+    return k.doc
