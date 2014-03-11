a50ef5e8f08247afa89312d1b610263f18600f11
Eric Kow <eric.kow@gmail.com>
2014-03-11 21:28:27 +0000

    RST to dependency tree conversions

---
+++ educe/rst_dt/deptree.py
@@ -0,0 +1,311 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Authors: Eric Kow
+# License: CeCILL-B (BSD3-like)
+
+"""
+Convert RST trees to dependency trees and back.
+"""
+
+from collections import defaultdict, namedtuple
+
+from nltk import Tree
+
+from educe.rst_dt.transform import SimpleRSTTree, Node
+
+
+_N = "Nucleus"
+_S = "Satellite"
+_R = "Root"
+
+
+class RstDtException(Exception):
+    """
+    Exceptions related to conversion between RST and DT trees.
+    The general expectation is that we only raise these on bad
+    input, but in practice, you may see them more in cases of
+    implementation error somewhere in the conversion process.
+    """
+    def __init__(self, msg):
+        super(RstDtException, self).__init__(msg)
+
+
+# pylint: disable=R0903, W0232
+class DepNode(namedtuple("DepNode_", "edu num")):
+    """
+    Dependency graph node
+    """
+    pass
+
+
+class RelDepNode(object):
+    """
+    Dependency graph node annotated with relation label for
+    incoming link (None for Root node)
+
+    Fields of interest
+
+    * edu - core content of this node
+    * num - which EDU this was (for EDU span)
+    * rel - relation label
+    """
+
+    def __init__(self, edu, num, rel):
+        self.edu = edu
+        self.num = num
+        self.rel = rel
+
+    def __str__(self):
+        relstr = (self.rel + ": ") if self.rel else ""
+        return relstr + str(self.edu) + (" [%d]" % self.num)
+
+    def __repr__(self):
+        return str(self)  # only for debugging
+# pylint: enable=R0903, W0232
+
+
+def relaxed_nuclearity_to_deptree(rst_tree):
+    """
+    Converts a `SimpleRSTTree` to a dependency tree
+    """
+
+    dlinks = defaultdict(list)
+
+    def walk(tree):
+        """
+        Walk down tree, adding dependency graph links to dlinks as we go.
+        Return/percolate the head node found in our descent
+        """
+        if len(tree) == 1:  # pre-terminal
+            edu = tree[0]
+            edu_num = tree.node.edu_span[0]
+            return DepNode(edu, edu_num)
+        else:
+            rel = tree.node.rel
+            left = tree[0]
+            right = tree[1]
+            nscode = "".join(kid.node.nuclearity[0] for kid in tree)
+            lhead = walk(left)
+            rhead = walk(right)
+
+            if nscode == "NS":
+                head = lhead
+                child = rhead
+            elif nscode == "SN":
+                head = rhead
+                child = lhead
+            elif nscode == "NN":
+                head = lhead
+                child = rhead
+            else:
+                raise RstDtException("Don't know how to handle %s trees" %
+                                     nscode)
+            dlinks[head].append((rel, child))
+            return head
+
+    def mk_tree(relnode):
+        """
+        Convert a link dictionary to dependency tree
+        """
+        rel, dnode = relnode
+        rnode = RelDepNode(dnode.edu, dnode.num, rel)
+        kids = map(mk_tree, dlinks.get(dnode, []))
+        return Tree(rnode, kids)
+
+    head = walk(rst_tree)  # build dlinks
+    return mk_tree((None, head))
+
+
+# pylint: disable=R0903, W0232
+class TreeParts(namedtuple("TreeParts_", "edu edu_span span rel kids")):
+    """
+    Partially built RST tree when converting from dependency tree
+    Kids here is nuclearity-annotated children
+    """
+    pass
+# pylint: enable=R0903, W0232
+
+
+def relaxed_nuclearity_from_deptree(dtree, multinuclear):
+    r"""
+    Given a dependency tree and a collection of relation labels,
+    to be interpreted as being multinuclear, return a 'SimpleRSTTree'.
+    Note that nodes are ordered by text span, so that `e1 -R-> e2` and
+    `e2 -R-> e1` would both be interpreted as `R(e1, e2)`, the
+    potential difference being which is treated as the nucleus and as
+    the satellite.
+
+    The conversion algorithm can be seen a two-pass descent/conversion,
+    ascent/assembly one. First, we go down the dependency tree,
+    annotating each node with a trivial RST tree (ie. a leaf node).
+    Then, we walk back up the dependency tree, connecting RST treelets
+    together, gradually replacing nodes with increasingly larger RST
+    trees.
+
+    The first tricky part of this conversion is to keep in mind
+    that the conversion process involves some rotation, namely
+    that this dependency structure ::
+
+           R
+        a ---> b
+
+    (if we gloss over nuclearity and node order details) is rotated
+    into something that looks like this ::
+
+
+        R +-> a                                 R
+          |          (or more conventionally,  / \ )
+          +-> b                               a   b
+
+
+    Where things get tricky is if a node in the dependency tree
+    points has multiple children ::
+
+            r1
+        src +--> tgt1
+            |
+            |r2
+            +--> tgt2
+            |
+            ..
+            |
+            |rN
+            +--> tgtN
+
+
+    In that case, having pivoted the `src(r1:tgt1)` link into something
+    like `r1(src, tgt)`, it may not be so clear how to deal with
+    subsequents targets `tgt2..tgtN`. We want to both reflect that `src`
+    has a dependency link with all of these targets at the same time
+    even though it's stuck as an RST child node. The trick is to see the
+    process as more of a fold than a map. Keeping in mind that `src`,
+    `tgt1..tgtN` are all RST treelets (`tgt1..tgtN` having been built up
+    from recursive descent), the idea is to gradually replace the src
+    node with an increasingly nested RST tree as we go along ::
+
+
+        +----------+
+        |   r1     |    r2
+        |  /  \    + -+---> tgt2
+        | src tgt1 |  |
+        +----------+  | r3
+                      +---> tgt3
+                      |
+                      ..
+                      | rN
+                      +---> tgtN
+
+
+
+        +------------+
+        |     r2     |
+        |    /  \    |
+        |   r1  tgt2 |    r3
+        |  /  \      + -+---> tgt3
+        | src tgt1   |  |
+        +------------+  ..
+                        | rN
+                        +---> tgtN
+
+
+    (and so on, until all we have left is a single RST tree).
+    """
+
+    def tgt_nuclearity(rel):
+        """
+        The target of a dep tree link is normally the satellite
+        unless the relation is marked multinuclear
+        """
+        return _N if rel.lower() in multinuclear else _S
+
+    def mk_leaf(dnode):
+        """
+        Trivial partial tree for use when processing dependency
+        tree leaves
+        """
+        return TreeParts(edu=dnode.edu,
+                         edu_span=(dnode.num, dnode.num),
+                         span=dnode.edu.text_span(),
+                         rel="leaf",
+                         kids=[])
+
+    def parts_to_tree(nuclearity, parts):
+        """
+        Combine root nuclearity information with a partial tree
+        to form a full RST `SimpleTree`
+        """
+        node = Node(nuclearity,
+                    parts.edu_span,
+                    parts.span,
+                    parts.rel)
+        kids = parts.kids or [parts.edu]
+        return SimpleRSTTree(node, kids)
+
+    def connect_trees(src, tgt, rel):
+        """
+        Return a partial tree, assigning order and nuclearity to
+        child trees
+        """
+        tgt_nuc = tgt_nuclearity(rel)
+
+        if src.span.overlaps(tgt.span):
+            raise RstDtException("Span %s overlaps with %s " %
+                                 (src.span, tgt.span))
+        elif src.span <= tgt.span:
+            left = parts_to_tree(_N, src)
+            right = parts_to_tree(tgt_nuc, tgt)
+        else:
+            left = parts_to_tree(tgt_nuc, tgt)
+            right = parts_to_tree(_N, src)
+
+        l_edu_span = left.node.edu_span
+        r_edu_span = right.node.edu_span
+
+        edu_span = (min(l_edu_span[0], r_edu_span[0]),
+                    max(l_edu_span[1], r_edu_span[1]))
+        res = TreeParts(edu=src.edu,
+                        edu_span=edu_span,
+                        span=src.span.merge(tgt.span),
+                        rel=rel,
+                        kids=[left, right])
+        return res
+
+    def walk(ancestor, subtree):
+        """
+        The basic descent/ascent driver of our conversion algorithm.
+        Note that we are looking at three layers of the dependency
+        tree at the same time.
+
+
+                     r0       r1
+            ancestor --> src +--> tgt1
+                             |
+                             |r2
+                             +--> tgt2
+                             |
+                             ..
+                             |
+                             |rN
+                             +--> tgtN
+
+        The base case is if src is a leaf node (no children),
+        whereupon we return a tiny tree connecting the two.
+
+        If we do have children, we have to first obtain the
+        full RST tree for src (through the folding process
+        described in the docstring for the main function)
+        before connecting it to its ancestor.
+        """
+        rel = subtree.node.rel
+        src = mk_leaf(subtree.node)
+        # descend into each child, but note that we are folding
+        # rather than mapping, ie. we threading along a nested
+        # RST tree as go from sibling to sibling
+        for tgt in subtree:
+            src = walk(src, tgt)
+        # ancestor is None in the case of the root node
+        return connect_trees(ancestor, src, rel) if ancestor else src
+
+    rparts = walk(None, dtree)
+    return parts_to_tree(_R, rparts)
