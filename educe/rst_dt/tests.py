13922febbb551a85b2269831f14162704e76316f
Eric Kow <eric.kow@gmail.com>
2014-03-13 11:10:47 +0000

    Some trivial RST/DT tests

--- educe/rst_dt/tests.py
+++ educe/rst_dt/tests.py
@@ -1,9 +1,10 @@
 import codecs
 import glob
+import os
 import sys
 import unittest
 
-from educe.rst_dt import parse, transform
+from educe.rst_dt import parse, transform, deptree
 
 # ---------------------------------------------------------------------
 # example tree snippets
@@ -66,16 +67,6 @@ TEXT1 = " ".join(\
          "and even condition certain types of paneling before installation. "
          ])
 
-TSTR2 = """
-( Root (span 1 6)
-  ( Nucleus (span 1 6) (rel2par span)
-    ( Satellite (leaf 1) (rel2par evaluation-s) (text _!The back of the Moth.<P>_!) )
-    ( Nucleus (span 2 6) (rel2par span)
-      ( Nucleus (span 2 4) (rel2par Same-Unit)
-        ( Nucleus (leaf 2) (rel2par span) (text _!Baron Bromley III,_!) )))))
-"""
-
-TEXT2 = "The back of the Moth.<P> Baron Bromley III,"
 
 # ---------------------------------------------------------------------
 #
@@ -83,6 +74,9 @@ TEXT2 = "The back of the Moth.<P> Baron Bromley III,"
 
 
 class RSTTest(unittest.TestCase):
+    _trees = {}  # will be lazily populated
+    longMessage = True
+
     def test_tstr0(self):
         parse.RSTTree.build(TSTR0)
 
@@ -94,25 +88,35 @@ class RSTTest(unittest.TestCase):
         self.assertEqual(TEXT1, t_text)
         self.assertEqual(len(t_text), sp.char_end)
 
-    def test_tstr2(self):
-        t = parse.RSTTree.build(TSTR2)
-        t_text = t.text()
-        sp = t.node.span
-        self.assertEqual((1, 6), t.edu_span())
-        self.assertEqual(TEXT2, t_text)
-        self.assertEqual(len(t_text), sp.char_end)
-
     def test_from_files(self):
         for i in glob.glob('tests/*.dis'):
             t = parse.read_annotation_file(i)
             self.assertEqual(len(t.text()), t.node.span.char_end)
 
+    def _test_trees(self):
+        if not self._trees:
+            self._trees = {}
+            for i, tstr in enumerate([TSTR0, TSTR1]):
+                self._trees["tstr%d" % i] = parse.RSTTree.build(tstr)
+            for i in glob.glob('tests/*.dis'):
+                bname = os.path.basename(i)
+                self._trees[bname] = parse.read_annotation_file(i)
+        return self._trees
+
     def test_binarize(self):
-        for tstr in [TSTR0, TSTR1, TSTR2]:
-            tree = parse.RSTTree.build(tstr)
-            bin_tree = transform.binarize(tree)
+        for name, tree in self._test_trees().items():
+            bin_tree = transform._binarize(tree)
             self.assertTrue(transform.is_binary(bin_tree))
-        for i in glob.glob('tests/*.dis'):
-            tree = parse.read_annotation_file(i)
-            bin_tree = transform.binarize(tree)
-            self.assertTrue(transform.is_binary(bin_tree))
+
+    def test_rst_to_dt(self):
+        for name, tree in self._test_trees().items():
+            rst1 = transform.SimpleRSTTree.from_rst_tree(tree)
+            dep = deptree.relaxed_nuclearity_to_deptree(rst1)
+            rst2 = deptree.relaxed_nuclearity_from_deptree(dep, [])
+            self.assertEqual(rst1.node.span,
+                             rst2.node.span,
+                             "span equality on " + name)
+            self.assertEqual(rst1.node.edu_span,
+                             rst2.node.edu_span,
+                             "edu span equality on " + name)
+
