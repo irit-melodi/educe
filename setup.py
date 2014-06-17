7f742a0f28a7212cecd52b60dc268a84166f0d52
Eric Kow <eric.kow@gmail.com>
2014-06-17 17:42:19 +0200

    Move glozz scripts to scripts dir

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from setuptools import setup, find_packages
+import glob
+
 setup(name='educe',
       version='0.1',
       author='Eric Kow',
       author_email='eric.kow@gmail.com',
       packages=['educe', 'educe.stac', 'educe.rst_dt', 'educe.pdtb', 'educe.external'],
-      scripts=['glozz', 'glozz-graph'],
+      scripts=glob.glob('scripts/*'),
       requires=['python_graph (>= 1.8.2)', 'pydot', 'python_graph_dot']
       )
