f58f5caaade50a116a83572aec6ac49db33f6d9a
Eric Kow <eric.kow@gmail.com>
2014-11-12 11:21:22 +0000

    educe.util: more powerful flag selection
    * add_corpus_filters now supports a choice_fields
      arg (:: Dict String String) that lets you limit
      a field to a particular subset
      (eg. you can only choose from GOLD or SILVER here)
    
    * without_fields lets you specify corpus selection
      fields to knock out
    
    * mk_is_interesting lets you 'preselect' the value
      for some fields (eg. is interesting if the stage
      is discourse)

--- educe/util.py
+++ educe/util.py
@@ -8,10 +8,6 @@ Miscellaneous utility functions
 from itertools import chain
 import re
 
-fileid_fields = [ 'stage', 'doc', 'subdoc', 'annotator' ]
-"""
-String representation of fields recognised in an educe.corpus.FileId
-"""
 
 def concat(items):
     ":: iter(iter(a)) -> iter(a)"
@@ -23,7 +19,22 @@ def concat_l(items):
     return list(chain.from_iterable(items))
 
 
-def add_corpus_filters(arg_parser, fields=fileid_fields):
+FILEID_FIELDS = ['stage', 'doc', 'subdoc', 'annotator']
+"""
+String representation of fields recognised in an educe.corpus.FileId
+"""
+
+
+def fields_without(unwanted):
+    """
+    Fields for `add_corpus_filters` without the unwanted members
+    """
+    return [x for x in FILEID_FIELDS if x not in unwanted]
+
+
+def add_corpus_filters(parser,
+                       fields=None,
+                       choice_fields=None):
     """
     For help with script-building:
 
@@ -31,44 +42,71 @@ def add_corpus_filters(arg_parser, fields=fileid_fields):
     the various attributes in a 'educe.corpus.FileId'
     (eg, document, annotator).
 
+    :param fields: which flag names to include (defaults
+    to `FILEID_FIELDS`)
+    :type fields: [String]
+
+    :param choice_fields: fields which accept a limited range
+    of answers
+    :type fields: Dict String [String]
+
     Meant to be used in conjunction with `mk_is_interesting`
     """
+    def add(flag, choices):
+        "add a flag"
+        parser.add_argument("--{}".format(flag),
+                            choices=choices,
+                            metavar="PY_REGEX" if choices is None else None,
+                            help="Limit to a particular {}(s)".format(field))
 
-    for x in fields:
-        arg_parser.add_argument( ('--%s' % x)
-                               , metavar='PY_REGEX'
-                               , help=('Limit to a particular %s(s)' % x)
-                               )
+    fields = FILEID_FIELDS if fields is None else fields
+    choice_fields = {} if choice_fields is None else choice_fields
 
-def mk_is_interesting(args):
+    for field, choices in choice_fields.items():
+        add(field, choices)
+
+    for field in fields:
+        if field not in choice_fields:
+            add(field, choices=None)
+
+
+def mk_is_interesting(args,
+                      preselected=None):
     """
     Return a function that when given a FileId returns 'True'
     if the FileId would be considered interesting according to
     the arguments passed in.
 
+    :param preselected: fields for which we already know what
+    matches we want
+    :type fields: Dict String [String]
+
     Meant to be used in conjunction with `add_corpus_filters`
     """
+
+    def mk_checker_generic(attr, pred):
+        "generalised helper for mk_checker"
+        def check(fileid):
+            "matching on k value"
+            val = fileid.__dict__[attr]
+            return False if val is None else pred(val)
+        return check
+
     def mk_checker(attr):
         """
         Given an attr name, return a function that checks a FileId
         to see if its attribution value matches the requested pattern.
         If the attribute was not requested, we skip the check.
         """
-
-        if args.__dict__.get(attr) is None:
-            return lambda _ : True
+        if attr in preselected:
+            pred = lambda v: v in preselected[attr]
+            return mk_checker_generic(attr, pred)
+        elif args.__dict__.get(attr) is None:
+            return lambda _: True
         else:
             argval = args.__dict__[attr]
             regex = re.compile(argval)
-            def check(fileid):
-                "regex matching on k value"
-                val = fileid.__dict__[attr]
-                if val is None:
-                    return False
-                else:
-                    return regex.match(val)
-            return check
+            return mk_checker_generic(attr, regex.match)
 
-    doc_checkers = [mk_checker(attr) for attr in
-                    ['stage', 'doc', 'subdoc', 'annotator']]
-    return lambda k : all(check(k) for check in doc_checkers)
+    doc_checkers = [mk_checker(attr) for attr in FILEID_FIELDS]
+    return lambda k: all(check(k) for check in doc_checkers)
