5866995eebe16ef8330dbdf84863b67533c3ed53
Eric Kow <eric.kow@gmail.com>
2014-11-18 19:01:20 +0000

    educe.stac.util.prettifyxml
    Parameterisable indent

--- educe/stac/util/prettifyxml.py
+++ educe/stac/util/prettifyxml.py
@@ -9,12 +9,13 @@ from xml.etree import ElementTree
 from xml.dom import minidom
 import sys
 
-def prettify(elem):
+
+def prettify(elem, indent=""):
     """Return a pretty-printed XML string for the Element.
     """
     rough_string = ElementTree.tostring(elem, 'utf-8')
     reparsed = minidom.parseString(rough_string.replace('\n', ''))
-    return reparsed.toprettyxml(indent="")
+    return reparsed.toprettyxml(indent=indent)
 
 if __name__ == '__main__':
     tree = ElementTree.parse(sys.argv[1])
