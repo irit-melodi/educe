4c0bc8721931ec7fa30d07efbaced6b35e011c32
Eric Kow <eric.kow@gmail.com>
2014-11-24 12:52:08 +0000

    Tighten up addressee prediction a bit
    Don't predict addresee if followed by a verb or
    preceeded by a preposition

--- educe/stac/learning/addressee.py
+++ educe/stac/learning/addressee.py
@@ -25,6 +25,24 @@ def is_emoticon(token):
     return token.tag == 'E'
 
 
+def is_verb(token):
+    "True if the token is tagged as a verb"
+    return token.tag == 'V'
+
+
+def is_preposition(token):
+    "True if the token is tagged as a preposition"
+    return token.tag == 'P'
+
+
+def _is_maybe(pred, lst, idx):
+    """
+    Given a predicate, a list, and an index, return True if
+    the item is in the list and the predicate is true for it
+    """
+    return pred(lst[idx]) if len(lst) > idx else False
+
+
 def guess_addressees_for_edu(contexts, players, edu):
     """
     return a set of possible addressees for the given EDU
@@ -36,19 +54,36 @@ def guess_addressees_for_edu(contexts, players, edu):
     """
     context = contexts[edu]
     players_from_lc = {x.lower(): x for x in players}  # orig case
-    interesting = [x.word.lower() for x in context.tokens
-                   if not (is_punct(x) or is_emoticon(x))]
-    if not interesting:
+
+    interesting_toks = [x for x in context.tokens
+                        if not (is_punct(x) or is_emoticon(x))]
+    interesting_words = [x.word.lower() for x in interesting_toks]
+
+    if not interesting_toks:
         return None
 
-    is_player = lambda x: x in players_from_lc
-    players_prefix = list(takewhile(is_player, interesting))
-    players_suffix = list(takewhile(is_player, reversed(interesting)))
+    def take_player_prefix(words):
+        """Return any player names at the start of a word
+        sequence ::
 
-    if players_prefix:
-        return frozenset(players_from_lc[x] for x in players_prefix)
-    elif players_suffix:
-        return frozenset(players_from_lc[x] for x in players_suffix)
+        [String] -> [String]
+        """
+        return [players_from_lc[x] for x in
+                takewhile(lambda x: x in players_from_lc, words)]
+
+    players_prefix = take_player_prefix(interesting_words)
+    players_suffix = take_player_prefix(reversed(interesting_words))
+
+    v_after_prefix = _is_maybe(is_verb, interesting_toks,
+                               len(players_prefix))
+    p_before_suffix = _is_maybe(is_preposition, interesting_toks,
+                                len(interesting_toks) -
+                                len(players_suffix) - 1)
+
+    if players_prefix and not v_after_prefix:
+        return players_prefix
+    elif players_suffix and not p_before_suffix:
+        return players_suffix
     else:
         return None
 
