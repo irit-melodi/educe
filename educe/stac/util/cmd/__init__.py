4eddb3053cb47fc3e7c690e7d86db84d563e3e74
Eric Kow <eric.kow@gmail.com>
2014-12-04 09:52:26 +0100

    Code style: group subcommands into sections

--- educe/stac/util/cmd/__init__.py
+++ educe/stac/util/cmd/__init__.py
@@ -9,22 +9,34 @@ stac-util subcommands
 # (we have a command called filter)
 from . import (clean_emoticons, count, filter, filter_graph, graph, insert,
                merge_dialogue, merge_edus, move, nudge, nudge_dialogue, rename,
-               rewrite, split_edu, text, tmp)
+               rewrite, split_edu, text, tmp, weave)
 
+# at the time of this writing argparse doesn't support a way to group
+# subcommands into sections, but maybe we can wait for it to grow such
+# a feature, or write our own formatter class, or just abuse the command
+# epilog
+SUBCOMMAND_SECTIONS =\
+    [('Querying',
+      [text,
+       count,
+       graph,
+       filter_graph]),
+     ('Editing',
+      [clean_emoticons,
+       filter,
+       rename,
+       rewrite,
+       merge_dialogue,
+       merge_edus,
+       split_edu,
+       nudge,
+       nudge_dialogue,
+       tmp]),
+     ('Advanced editing',
+      [insert,
+       weave,
+       move])]
 
-SUBCOMMANDS = [clean_emoticons,
-               text,
-               count,
-               graph,
-               filter,
-               filter_graph,
-               rename,
-               rewrite,
-               merge_dialogue,
-               merge_edus,
-               split_edu,
-               nudge,
-               nudge_dialogue,
-               insert,
-               tmp,
-               move]
+SUBCOMMANDS = []
+for descr, section in SUBCOMMAND_SECTIONS:
+    SUBCOMMANDS.extend(section)
