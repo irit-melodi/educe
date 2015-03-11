44ba49cb12ab8b8846dc3ca4c9f49b4ee11d9a20
Eric Kow <eric.kow@gmail.com>
2015-03-11 11:28:49 +0100

    MAINT cut unused Purpose from educe.learning.keys
    We're not quite ready to get rid of the KeyGroups yet but
    at least we can start thinnning the code out to see more
    clearly in the long term

--- educe/learning/keys.py
+++ educe/learning/keys.py
@@ -46,135 +46,41 @@ class Substance(object):
             raise ValueError("Unknown substance " + substance)
 
 
-class Purpose(object):
-    """
-    A key can have one of three purposes:
-
-        * feature: feature for learning tasks
-        * meta: some sort of indexing feature (eg. id)
-        * class: the thing we are trying to learn (normally only one
-          key has this purpose)
-
-    If we ever reach a point where we're happy to switch to Python 3
-    wholesale, we should subclass Enum
-    """
-    FEATURE = 1
-    META = 2
-    CLASS = 3
-
-    @classmethod
-    def to_orange(cls, purpose):
-        """
-        Orange-compatible string representation
-        """
-        if purpose is cls.FEATURE:
-            return ""
-        elif purpose is cls.META:
-            return "m"
-        elif purpose is cls.CLASS:
-            return "class"
-        else:
-            raise ValueError("Unknown purpose" + purpose)
-
-
-class HeaderType(object):
-    """
-    For output files
-
-    If we ever reach a point where we're happy to switch to Python 3
-    wholesale, we should subclass Enum
-    """
-    OLD_CSV = 1
-    NAME = 2
-    SUBSTANCE = 3
-    PURPOSE = 4
-
-
 class Key(object):
     """
     Feature name plus a bit of metadata
     """
 
 # pylint: disable=pointless-string-statement
-    def __init__(self, substance, purpose, name, description):
+    def __init__(self, substance, name, description):
         """
         You probably want to use the methods `continuous`,
         or `discrete` instead
         """
         self.substance = substance
         "see `Substance`"
-        self.purpose = purpose
-        "see `Purpose`"
         self.name = name
         self.description = description
 # pylint: disable=pointless-string-statement
 
-    def keycode(self):
-        """
-        A short code indicating the purpose and substance of the
-        feature.
-
-        If you stay within continuous/discrete for non-meta features,
-        this will be compatible with those recognised by the Orange
-        CSV reader.
-        """
-        if self.purpose is Purpose.CLASS:
-            return "c"
-        elif self.purpose is Purpose.META:
-            return "m"
-        elif self.purpose is Purpose.FEATURE:
-            if self.substance is Substance.CONTINUOUS:
-                return "C"
-            elif self.substance is Substance.DISCRETE:
-                return "D"  # not the same as that used in tab format (d)
-            elif self.substance is Substance.BASKET:
-                return "basket"
-            else:
-                oops = "Unknown substance {} in key {}".format(self.substance,
-                                                               self.name)
-                raise Exception(oops)
-        else:
-            oops = "Unknown purpose {} in key {}".format(self.purpose,
-                                                         self.name)
-            raise Exception(oops)
-
-    def to_csv(self):
-        "CSV-compatible string representation of this key"
-        return self.keycode() + "#" + self.name
-
     @classmethod
-    def continuous(cls, name, description, purpose=None):
+    def continuous(cls, name, description):
         "A key for fields that have range value (eg. numbers)"
-        purpose = purpose or Purpose.FEATURE
-        return cls(Substance.CONTINUOUS, purpose, name, description)
+        return cls(Substance.CONTINUOUS, name, description)
 
     @classmethod
-    def meta(cls, name, description, substance=None):
-        """
-        A key for fields that are used for indexing only.
-
-        Slightly deprecated. You should really use `discrete`
-        instead and specify that purpose=Purpose.META. But it's not a
-        big deal.
-        """
-        substance = substance or Substance.STRING
-        return cls(substance, Purpose.META, name, description)
-
-    @classmethod
-    def discrete(cls, name, description, purpose=None):
+    def discrete(cls, name, description):
         "A key for fields that have a finite set of possible values"
-        purpose = purpose or Purpose.FEATURE
-        return cls(Substance.DISCRETE, purpose, name, description)
+        return cls(Substance.DISCRETE, name, description)
 
     @classmethod
-    def basket(cls, name, description, purpose=None):
+    def basket(cls, name, description):
         """
         A key for fields that represent a multiset of possible values.
         Baskets should be dictionaries from string to int
         (collections.Counter would be a good bet for collecting these)
         """
-        purpose = purpose or Purpose.FEATURE
-        return cls(Substance.BASKET, purpose, name, description)
+        return cls(Substance.BASKET, name, description)
 
 
 class MagicKey(Key):
@@ -183,45 +89,30 @@ class MagicKey(Key):
     The goal of the magic key is to reduce the amount of boilerplate
     needed to define keys
     """
-    def __init__(self, substance, purpose, function):
+    def __init__(self, substance, function):
         name = re.sub("^feat_", "", function.__name__)
         description = function.__doc__
-        super(MagicKey, self).__init__(substance, purpose, name, description)
+        super(MagicKey, self).__init__(substance, name, description)
         self.function = function
 
     @classmethod
-    def continuous_fn(cls, function, purpose=None):
+    def continuous_fn(cls, function):
         "A key for fields that have range value (eg. numbers)"
-        purpose = purpose or Purpose.FEATURE
-        return cls(Substance.CONTINUOUS, purpose, function)
+        return cls(Substance.CONTINUOUS, function)
 
     @classmethod
-    def meta_fn(cls, function, substance=None):
-        """
-        A key for fields that are used for indexing only.
-
-        Slightly deprecated. You should really use `discrete`
-        instead and specify that purpose=Purpose.META. But it's not a
-        big deal.
-        """
-        substance = substance or Substance.STRING
-        return cls(substance, Purpose.META, function)
-
-    @classmethod
-    def discrete_fn(cls, function, purpose=None):
+    def discrete_fn(cls, function):
         "A key for fields that have a finite set of possible values"
-        purpose = purpose or Purpose.FEATURE
-        return cls(Substance.DISCRETE, purpose, function)
+        return cls(Substance.DISCRETE, function)
 
     @classmethod
-    def basket_fn(cls, function, purpose=None):
+    def basket_fn(cls, function):
         """
         A key for fields that represent a multiset of possible values.
         Baskets should be dictionaries from string to int
         (collections.Counter would be a good bet for collecting these)
         """
-        purpose = purpose or Purpose.FEATURE
-        return cls(Substance.BASKET, purpose, function)
+        return cls(Substance.BASKET, function)
 
 
 class KeyGroup(dict):
