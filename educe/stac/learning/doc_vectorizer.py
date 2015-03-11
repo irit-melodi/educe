ea026cc9c4560fec69f9ce120f34dcb6756f9108
Eric Kow <eric.kow@gmail.com>
2015-03-11 11:22:37 +0100

    MAINT cut unused dummy code

--- educe/stac/learning/doc_vectorizer.py
+++ educe/stac/learning/doc_vectorizer.py
@@ -1,17 +1,11 @@
 """This submodule implements document vectorizers"""
 
-import itertools as itr
-import numbers
-
-from collections import defaultdict, Counter
-
-from educe.util import (concat)
-
 # pylint: disable=too-few-public-methods
 
 UNK = '__UNK__'
 ROOT = 'ROOT'
 UNRELATED = 'UNRELATED'
+
 
 class DialogueActVectorizer(object):
     """Dialogue act extractor for the STAC corpus."""
@@ -63,22 +57,3 @@ class LabelVectorizer(object):
             for pair in self.instance_generator(doc):
                 label = doc.relations.get(pair, UNRELATED)
                 yield self.labelset_[label]
-
-
-class DummyFeatureVectorizer(object):
-    """Label extractor for the RST-DT treebank."""
-
-    def __init__(self, instance_generator):
-        """
-        instance_generator to enumerate the instances from a doc
-        """
-        self.instance_generator = instance_generator
-
-    def fit_transform(self, raw_documents):
-        """Learn the label encoder and return a vector of labels
-
-        There is one label per instance extracted from raw_documents.
-        """
-        # run through documents to generate y
-        for _ in concat(self.instance_generator(d) for d in raw_documents):
-            yield [(1, 1)]
