1b61e60037963f0c103b0578217571862bea918f
Eric Kow <eric.kow@gmail.com>
2015-05-11 08:46:06 +0200

    FIX type error in edu.stac.sanity.checks.graph

--- educe/stac/sanity/checks/graph.py
+++ educe/stac/sanity/checks/graph.py
@@ -46,7 +46,7 @@ def search_graph_edus(inputs, k, gra, pred):
     contexts = inputs.contexts[k]
     edu_names = {gra.annotation(name):name for name in gra.edus()}
     sorted_edus = sorted_first_widest(edu_names.keys())
-    return [UnitItem(doc, contexts, gra.annotation(x))
+    return [UnitItem(doc, contexts, x)
             for x in sorted_edus if pred(gra, contexts, edu_names[x])]
 
 
