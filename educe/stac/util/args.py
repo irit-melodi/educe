727eb5d71897721cdfff8b0c41c82193d024a2f6
Eric Kow <eric.kow@gmail.com>
2015-05-18 13:58:18 +0200

    FIX account for utils with no annotator

--- educe/stac/util/args.py
+++ educe/stac/util/args.py
@@ -44,7 +44,7 @@ def check_easy_settings(args):
             sys.exit("I don't know about any document called " + args.doc)
 
     guess_report = "{corpus} --doc \"{doc}\""
-    if args.annotator is None:
+    if 'annotator' in args.__dict__ and args.annotator is None:
         args.annotator = METAL_STR
         guess_report += ' --annotator "{}"'.format(args.annotator)
 
