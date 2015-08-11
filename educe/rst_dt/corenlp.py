c06ecb31da7b25355ce18ea318da0a5649aa4a56
moreymat <mathieu.morey@gmail.com>
2015-08-11 17:15:20 +0200

    ENH new module educe.rst_dt.corenlp

---
+++ educe/rst_dt/corenlp.py
@@ -0,0 +1,9 @@
+"""Functions to get parses on the RST-DT corpus using Stanford CoreNLP.
+"""
+
+from __future__ import print_function
+
+# TODO using educe.stac.corenlp, educe.external.corenlp
+# and Eric's rstdt-corenlp ipython notebook
+
+
