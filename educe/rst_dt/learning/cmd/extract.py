irit-melodi/educe

commit da4f414dc5bdb0f5a8f99568f350181536860710

MAINT educe.rst_dt minor code cleaning

educe/rst_dt/learning/cmd/extract.py
@@ -63,10 +63,20 @@ def config_argparser(parser):
                         '(when extracting test data, you may want to '
                         'use the feature vocabulary from the training '
                         'set ')
+    # labels
+    # TODO restructure ; the aim is to have three options:
+    # * fine-grained labelset (no transformation from treebank),
+    # * coarse-grained labelset (mapped from fine-grained),
+    # * manually specified list of labels (important here is the order
+    # of the labels, that implicitly maps labels as strings to integers)
+    # ... but what to do is not 100% clear right now
     parser.add_argument('--labels',
                         metavar='FILE',
                         help='Read label set from given feature file '
                         '(important when extracting test data)')
+    parser.add_argument('--coarse',
+                        action='store_true',
+                        help='use coarse-grained labels')
     # NEW use CoreNLP's output for tokenization and syntax (+coref?)
     parser.add_argument('--corenlp_out_dir', metavar='DIR',
                         help='CoreNLP output directory')
@@ -99,12 +109,10 @@ def main(args):
 
     # RST data
     # fileX docs are currently not supported by CoreNLP
-    if args.corenlp_out_dir:
-        exclude_file_docs = True
-    else:
-        exclude_file_docs = False
+    exclude_file_docs = args.corenlp_out_dir
 
-    rst_reader = RstDtParser(args.corpus, args, coarse_rels=True,
+    rst_reader = RstDtParser(args.corpus, args,
+                             coarse_rels=args.coarse,
                              exclude_file_docs=exclude_file_docs)
     rst_corpus = rst_reader.corpus
     # TODO: change educe.corpus.Reader.slurp*() so that they return an object
@@ -225,11 +233,9 @@ def main(args):
     else:
         of_bn = os.path.join(args.output, os.path.basename(args.corpus))
         out_file = '{}.relations{}'.format(of_bn, of_ext)
-
-    # dump EDUs and features in svmlight format
-    dump_all(X_gen, y_gen, out_file, labtor.labelset_, docs,
-             instance_generator)
-
     # dump vocabulary
     vocab_file = out_file + '.vocab'
     dump_vocabulary(vzer.vocabulary_, vocab_file)
+    # dump EDUs and features in svmlight format
+    dump_all(X_gen, y_gen, out_file, labtor.labelset_, docs,
+             instance_generator)

educe/rst_dt/corpus.py
@@ -168,12 +168,12 @@ class RstDtParser(object):
 
         Parameters
         ----------
-        doc_key : string ?
-            Identifier in the corpus of the document we want to decode.
+        doc_key: string ?
+            Identifier (in corpus) of the document we want to decode.
 
         Returns
         -------
-        doc : DocumentPlus
+        doc: DocumentPlus
             Bunch of information about this document notably its list of
             EDUs and the structures defined on them: RSTTree,
             SimpleRSTTree, RstDepTree.

educe/rst_dt/learning/doc_vectorizer.py
@@ -22,9 +22,18 @@ class DocumentLabelExtractor(object):
         self.labelset = labelset
 
     def _extract_labels(self, doc):
-        """Extract a label for each EDU pair extracted from doc
+        """Extract a label for each EDU pair extracted from `doc`.
 
-        Returns ([edu_pairs], [labels])
+        Parameters
+        ----------
+        doc: DocumentPlus
+            Rich representation of the document.
+
+        Returns
+        -------
+        labels: list of strings or None
+            List of labels, one for every pair of EDUs (in the order
+            in which they are generated by `self.instance_generator()`).
         """
         edu_pairs = self.instance_generator(doc)
         # extract one label per EDU pair
@@ -35,32 +44,25 @@ class DocumentLabelExtractor(object):
         """Extract label of instances, restricted to labelset"""
         labelset = self.labelset_
         # unknown labels
-        unk_lab = self.unknown_label
-        unk_lab_id = labelset[unk_lab]
+        unk_lab_id = labelset[self.unknown_label]
 
         analyze = self.build_analyzer()
         for doc in raw_documents:
             doc_labels = analyze(doc)
             for lab in doc_labels:
-                try:
-                    lab_id = labelset[lab]
-                    yield lab_id
-                except KeyError:
-                    yield unk_lab_id
+                yield labelset.get(lab, unk_lab_id)
 
     def _learn_labelset(self, raw_documents, fixed_labelset):
         """Learn the labelset"""
-        unk_lab = self.unknown_label
-
         if fixed_labelset:
             labelset = self.labelset_
-            unk_lab_id = labelset[unk_lab]
         else:
             # add a new value when a new label is seen
             labelset = defaultdict()
             labelset.default_factory = labelset.__len__
-            # the id of the unknown label should be 0
-            unk_lab_id = labelset[unk_lab]
+        # the id of the unknown label should be 0
+        unk_lab = self.unknown_label
+        unk_lab_id = labelset[unk_lab]
 
         analyze = self.build_analyzer()
         for doc in raw_documents:
@@ -80,9 +82,20 @@ class DocumentLabelExtractor(object):
         return labelset
 
     def decode(self, doc):
-        """Decode the input into a DocumentPlus
+        """Currently a no-op if doc is a DocumentPlus.
 
-        doc is an educe.corpus.FileId
+        Raises an exception otherwise.
+        Was: Decode the input into a DocumentPlus.
+
+        Parameters
+        ----------
+        doc: DocumentPlus
+            Rich representation of the document.
+
+        Returns
+        -------
+        doc: DocumentPlus
+            Rich representation of `doc`.
         """
         if not isinstance(doc, DocumentPlus):
             # doc = self.decoder(doc)

commit feb68498106ee2f0ca43609d54688c9a3af25f64

FIX rst_dt.learning.cmd.extract dump vocabulary as final stage
This fixes a regression introduced by commit da4f414d .

educe/rst_dt/learning/cmd/extract.py
@@ -233,9 +233,9 @@ def main(args):
     else:
         of_bn = os.path.join(args.output, os.path.basename(args.corpus))
         out_file = '{}.relations{}'.format(of_bn, of_ext)
-    # dump vocabulary
-    vocab_file = out_file + '.vocab'
-    dump_vocabulary(vzer.vocabulary_, vocab_file)
     # dump EDUs and features in svmlight format
     dump_all(X_gen, y_gen, out_file, labtor.labelset_, docs,
              instance_generator)
+    # dump vocabulary
+    vocab_file = out_file + '.vocab'
+    dump_vocabulary(vzer.vocabulary_, vocab_file)